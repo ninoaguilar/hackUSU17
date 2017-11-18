f17298d2b780b2feba612831b4061d5598bd4dbb
Zenaido <luis.hernandez@aggiemail.usu.edu>
2017-11-17 22:42:51 -0700

    fixed platforms

--- js/updatePlayer.js
+++ js/updatePlayer.js
@@ -1,4 +1,5 @@
 function updatePlayer() {
+
   //  Collide the player and the stars with the platforms
   game.physics.arcade.collide(player, platforms);
   game.physics.arcade.collide(stars, platforms);
@@ -11,55 +12,68 @@ function updatePlayer() {
   //  Reset the players velocity (movement)
   player.body.velocity.x = 0;
 
-  if (cursors.left.isDown)
-  {
-      //  Move to the left
-      player.body.velocity.x = -150;
+  if (cursors.left.isDown) {
+    //  Move to the left
+    player.body.velocity.x = -150;
 
-      player.animations.play('left');
-  }
-  else if (cursors.right.isDown)
-  {
-    if(player.position.x < 500){
+    player.animations.play('left');
+  } else if (cursors.right.isDown) {
+    if (player.position.x < 500) {
       player.body.velocity.x = 150;
-    }else{
-      counter+=1;
-      if(counter % 50 == 0){
-        count = 1;
 
-        var badGuy = enemies.create(1200,300,'baddie',1);
-        badGuy.animations.add('left',[0,1],10,true);
+    } else {
+      counter += 1;
+      updatePlatforms();
+      if (counter % 100 == 0) {
+        console.log("plat created");
+
+        var badGuy = enemies.create(1200, 300, 'baddie', 1);
+
+
+        badGuy.animations.add('left', [0, 1], 10, true);
         badGuy.body.gravity.y = 300;
         badGuy.body.velocity.x = -150;
 
-        enemies.forEach(function(enemy){
-          enemy.animations.play('left');
-          if(enemy.position.x < 0){
-            enemy.kill();
-          }
-        },this)
+
       }
-      if(counter % 200 == 0){
-        var plat = platforms.create(1200,  Math.floor(Math.random() * 20) + 300, 'ground');
-        
+      if (counter % 200 == 0) {
+        var platform = platforms.create(1200, Math.floor(Math.random() * 20) + 300, 'ground');
+
+        platform.body.immovable = true;
       }
+
     }
-      //  Move to the right
 
+    player.animations.play('right');
+    //  Move to the right
 
-      player.animations.play('right');
-  }
-  else
-  {
-      //  Stand still
-      player.animations.stop();
+  } else {
+    //  Stand still
+    player.animations.stop();
 
-      player.frame = 1;
+    player.frame = 1;
   }
 
   //  Allow the player to jump if they are touching the ground.
-  if (cursors.up.isDown && player.body.touching.down)
-  {
-      player.body.velocity.y = -350;
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.body.velocity.y = -350;
   }
+}
+
+
+
+function updatePlatforms() {
+  platforms.forEach(function(platform) {
+    platform.position.x -= 150 / 70;
+  }, this)
+};
+
+
+function updateEnemies() {
+  enemies.forEach(function(enemy) {
+    enemy.animations.play('left');
+    if (enemy.position.x < 0) {
+      enemy.kill();
+    }
+  }, this)
 };
